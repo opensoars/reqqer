426f2cbbf4e3327f593f1e7ced411fd30c81298e
opensoars <sam_1700@hotmail.com>
2015-12-23 17:37:23 +0100

    Basic libs usage tests

--- test/mocha/resFromReqArr.js
+++ test/mocha/resFromReqArr.js
@@ -7,6 +7,13 @@ describe('resFromReqArr', function () {
     assert.equal(typeof resFromReqArr, 'function');
   });
 
+  it('returns an object containing key value module pairs according to passed array elements', function () {
+    var res = resFromReqArr(['http', 'https']);
+
+    assert.equal(typeof res.http, 'object');
+    assert.equal(typeof res.https, 'object');
+  });
+
   it('throws when it\'s passed an array with elements of not type string', function (done) {
     try {
       resFromReqObj([123]);
--- test/mocha/resFromReqObj.js
+++ test/mocha/resFromReqObj.js
@@ -7,6 +7,16 @@ describe('resFromReqObj', function () {
     assert.equal(typeof resFromReqObj, 'function');
   });
 
+  it('returns an object containing key value module pairs according to object key value pairs', function () {
+    var res = resFromReqObj({
+      http_test: 'http',
+      https_test: 'https'
+    });
+
+    assert.equal(typeof res.http_test, 'object');
+    assert.equal(typeof res.https_test, 'object');
+  });
+
   it('throws when it\'s passed an object with values of not type string', function (done) {
     try {
       resFromReqObj({ test: 123 });
