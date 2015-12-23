4f3ad900268540c66d520b712d19ee34566c5945
opensoars <sam_1700@hotmail.com>
2015-12-23 17:27:32 +0100

    Basic lib require tests

--- test/mocha/resFromReqObj.js
+++ test/mocha/resFromReqObj.js
@@ -2,8 +2,15 @@ var assert = require('assert');
 
 describe('resFromReqObj', function () {
   var resFromReqObj = require('./../../lib/resFromReqObj');
-  
+
   it('returns a function when it\'s required', function () {
     assert.equal(typeof resFromReqObj, 'function');
   });
+
+  try {
+    resFromReqObj([1, 2, 3]);
+  }
+  catch (e) {
+    console.log(e);
+  }
 });
